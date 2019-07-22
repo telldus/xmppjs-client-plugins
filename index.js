350da39854fafcc369b36cee8d945883e4210c0f
Rimnesh Fernandez <rimnesh.fernandez@gmail.com>
2019-07-22 13:38:45 +0530

    Format code

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import setupIBB from "./packages/xep-0047-ibb";
-import setupSI from "./packages/xep-0096-si";
+import setupIBB from './packages/xep-0047-ibb';
+import setupSI from './packages/xep-0096-si';
 
 module.exports = {
-    setupIBB,
-    setupSI,
+	setupIBB,
+	setupSI,
 };
